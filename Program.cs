569f1193d49e950fcbc6087a9f10e6a74f83fbce
Andre Felipe <andrefelipe.pi2@gmail.com>
2023-02-28 22:03:04 -0300

    Estrutura condicional

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LerNegativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Digite um numero: ");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x >= 0)
+            {
+                Console.WriteLine("Nao negativo");
+            }
+            else 
+            {
+                Console.WriteLine("Negativo");
+            }
+        }
+    }
+}
